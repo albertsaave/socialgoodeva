712de5ebf34e872c2f1fdb9654797c40c04b5d2a
Hugh Kolias <hughkolias@gmail.com>
2017-03-31 22:19:06 -0400

    hugh: more

--- src/App.js
+++ src/App.js
@@ -91,51 +91,23 @@ class App extends Component {
                     <p style={{color: '#f68d2e'}}> Receive an appreciation gift, cancel anytime. </p>
                   }
                   { this.state.oneTimePayments &&
-                    <p style={{color: '#f68d2e'}}> Thank you for helping us end youth homelessness. </p>
+                    <p style={{color: '#f68d2e'}}> Thank you! </p>
                   }
                 <h5 style={{color:'#585858', margin: '0 0 10px 0', fontSize: '25px'}}> Amount </h5>
-                <div>
-                  <RaisedButton label="$50"
-                   style={{marginRight: '10px', border: '1px solid #3eb1c8', boxShadow: 'none', borderRadius: '3px'}}
-                   primary={this.state.fiftyDollars}
-                   onTouchTap={() => this.setState({donationAmount: 50, fiftyDollars: true, hundredDollars: false,
-                    hundredFiftyPayments: false, otherPayments: false, showCustomAmount: false})}
-                   />
-                  <RaisedButton label="$100"
-                    style={{marginRight: '0px', border: '1px solid #3eb1c8', boxShadow: 'none', borderRadius: '3px'}}
-                    primary={this.state.hundredDollars}
-                    onTouchTap={() => this.setState({donationAmount: 100, fiftyDollars: false, hundredDollars: true,
-                     hundredFiftyPayments: false, otherPayments: false, showCustomAmount: false})}
+                  <div>
+                    <span style={{color:'#585858', fontSize: '25px', verticalAlign: 'text-bottom'}}>$</span>
+                    <TextField
+                      style={{color: 'black', marginTop: '-20px', fontSize: '35px', width: '260px', padding: '5px'}}
+                      value={this.state.donationAmount}
+                      onChange={this.handleChange}
                     />
-                </div>
-                <div>
-                  <RaisedButton label="$150"
-                    style={{marginRight: '10px', border: '1px solid #3eb1c8', boxShadow: 'none', borderRadius: '3px'}}
-                    primary={this.state.hundredFiftyPayments}
-                    onTouchTap={() => this.setState({donationAmount: 150, fiftyDollars: false, hundredDollars: false,
-                     hundredFiftyPayments: true, otherPayments: false, showCustomAmount: false})}
-                    />
-                  <RaisedButton label="Other"
-                    style={{marginRight: '0px', marginTop: '5px', border: '1px solid #3eb1c8 ', boxShadow: 'none', borderRadius: '3px'}}
-                    onClick={this.handleClick}
-                    onTouchTap={() => this.setState({fiftyDollars: false, hundredDollars: false,
-                     hundredFiftyPayments: false, otherPayments: true})}
-                    primary={this.state.otherPayments}/>
                   </div>
-                {  this.state.showCustomAmount &&
-                  <TextField
-                    style={{color: 'black'}}
-                    floatingLabelText="Donation Amount"
-                    value={this.state.donationAmount}
-                    onChange={this.handleChange}
-                  />
-                }
                 </div>
                 }
                 <div className='row'>
-                  <RaisedButton secondary={true} label="Submit" style={{width: '100%', marginTop: '20px', backgroundColor: '#3eb1c8', height: '46px'}}
+                  <RaisedButton secondary={true} label="Submit" style={{width: '100%', marginTop: '12px', backgroundColor: '#3eb1c8', height: '46px'}}
                     label={<span>
-                      <p style={{zIndex: '-1', margin: '-3px'}}> Donate ${this.state.donationAmount}{this.showIfMonthly()} </p>
+                      <p style={{zIndex: '-1', margin: '-6px', fontSize: '19px'}}> Donate ${this.state.donationAmount}{this.showIfMonthly()} </p>
                       <StripeCheckout
                         token={this.onToken}
                         stripeKey="pk_test_dZTC0BYbhboAzM3HuSRAd3RC"
