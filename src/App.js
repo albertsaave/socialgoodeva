90116f4e844957c766f237cb6741c34c9ab1f1b0
Hugh Kolias <hughkolias@gmail.com>
2017-04-01 04:00:31 -0400

    hugh: ajja

--- src/App.js
+++ src/App.js
@@ -159,10 +159,10 @@ class App extends Component {
 
           </div>
 
-          <div style={{width: '300px', margin: 'auto'}}>
+          <div style={{width: '300px', margin: 'auto', marginBottom: '30px'}}>
             <Card style={style.mainCard}>
               { this.state.showPaymentPage &&
-                <div style={{marginBottom: '30px'}}>
+                <div>
                 <div>
                 <RaisedButton label="Monthly"
                   primary={this.state.monthlyPayments}
@@ -227,7 +227,7 @@ class App extends Component {
               }
 
               { this.state.showThankYouPage &&
-                <div style={{margin: 'auto', marginTop: '15px', maxWidth: '500px', marginBottom: '30px'}}>
+                <div style={{margin: 'auto', marginTop: '15px', maxWidth: '500px'}}>
                   <img src='./images/thankyoushare.jpg' style={{marginLeft: '-7px', height: '300px', width:'300px', marginTop: '10px', marginBottom: '15px'}}/>
 
                  <div style={{width: '100%', color: '#404040'}}>
